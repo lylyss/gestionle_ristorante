6ff2caf77a21cbe64569d3a5b39f4a93b31f1725
Ren-Mor <renatomorelli@hotmail.it>
2025-11-14 16:17:52 +0100

    fix

--- gestionale_ristorante/src/components/MyNav.tsx
+++ gestionale_ristorante/src/components/MyNav.tsx
@@ -8,32 +8,34 @@ function MyNav() {
       bg="dark"
       data-bs-theme="dark"
     >
-      <Navbar.Brand href="/" className="mb-4 fs-2 fw-bold d-flex justify-content-center align-items-center">
-        Bistrot Ristorante
-      </Navbar.Brand>
-      <div className="flex-grow-1 d-flex align-items-center mynav-navcontainer">
-        <nav className="overflow-auto mynav-nav">
-          <ul className="d-flex gap-3 mb-1 mynav-list">
-            <li className="mynav-li">
-              <h6 className="text-white text-center mb-0">Primi</h6>
-            </li>
-            <li className="mynav-li">
-              <h6 className="text-white text-center mb-0">Secondi</h6>
-            </li>
-            <li className="mynav-li">
-              <h6 className="text-white text-center mb-0">Contorni</h6>
-            </li>
-            <li className="mynav-li">
-              <h6 className="text-white text-center mb-0">Dessert</h6>
-            </li>
-            <li className="mynav-li">
-              <h6 className="text-white text-center mb-0">Soft drinks</h6>
-            </li>
-            <li className="mynav-li">
-              <h6 className="text-white text-center mb-0">Alcolici</h6>
-            </li>
-          </ul>
-        </nav>
+      <div className="overflow-auto">
+        <Navbar.Brand href="/" className="mb-4 fs-2 fw-bold d-flex justify-content-center align-items-center">
+          Bistrot Ristorante
+        </Navbar.Brand>
+        <div className="flex-grow-1 d-flex align-items-center mynav-navcontainer">
+          <nav className="overflow-auto mynav-nav">
+            <ul className="d-flex gap-3 mb-1 mynav-list">
+              <li className="mynav-li">
+                <h6 className="text-white text-center mb-0">Primi</h6>
+              </li>
+              <li className="mynav-li">
+                <h6 className="text-white text-center mb-0">Secondi</h6>
+              </li>
+              <li className="mynav-li">
+                <h6 className="text-white text-center mb-0">Contorni</h6>
+              </li>
+              <li className="mynav-li">
+                <h6 className="text-white text-center mb-0">Dessert</h6>
+              </li>
+              <li className="mynav-li">
+                <h6 className="text-white text-center mb-0">Soft drinks</h6>
+              </li>
+              <li className="mynav-li">
+                <h6 className="text-white text-center mb-0">Alcolici</h6>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </Navbar>
   );
