74efe3b45d72c0e606c6279180ff8cbf6038200f
Ren-Mor <renatomorelli@hotmail.it>
2025-11-14 11:49:16 +0100

    Fix pagina home

--- gestionale_ristorante/src/components/MyHero.tsx
+++ gestionale_ristorante/src/components/MyHero.tsx
@@ -7,11 +7,10 @@ import "../App.css";
 function Hero() {
   return (
     <>
-      <section className="text-border hero-background text-white text-center d-flex flex-column justify-content-center align-items-center"></section>
+      <section className="text-border hero-background text-white d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-center">Benvenuti al Bistrot</h1>
+      </section>
       <Container>
-        <div className="d-flex justify-content-center mt-5">
-          <h1>Benvenuti al Bistrot</h1>
-        </div>
         <div className="fixed-bottom d-flex justify-content-center mb-5">
           <NavLink className="mb-4" to="/menupage">
             <Button className="btn btn-secondary mb-5 fs-4">Sfoglia il menu</Button>
