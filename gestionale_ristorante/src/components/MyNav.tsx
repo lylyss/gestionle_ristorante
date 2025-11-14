9a1c7c0503aa07eca0ecd9c8930d2796d200d72d
Ren-Mor <renatomorelli@hotmail.it>
2025-11-14 16:15:33 +0100

    fix stile navbar (css è il demonio)

--- gestionale_ristorante/src/components/MyNav.tsx
+++ gestionale_ristorante/src/components/MyNav.tsx
@@ -2,13 +2,18 @@ import { Navbar } from "react-bootstrap";
 
 function MyNav() {
   return (
-    <Navbar expand="xl" className="py-4 px-5 fixed-top text-nowrap" bg="dark" data-bs-theme="dark">
-      <Navbar.Brand href="/" className="mb-3 fs-2 fw-bold d-flex justify-content-center w-100">
+    <Navbar
+      expand="lg"
+      className="py-4 px-5 fixed-top text-nowrap d-flex flex-row justify-content-center align-items-center"
+      bg="dark"
+      data-bs-theme="dark"
+    >
+      <Navbar.Brand href="/" className="mb-4 fs-2 fw-bold d-flex justify-content-center align-items-center">
         Bistrot Ristorante
       </Navbar.Brand>
       <div className="flex-grow-1 d-flex align-items-center mynav-navcontainer">
-        <nav className="w-100 overflow-auto mynav-nav">
-          <ul className="d-flex gap-3 mb-2 mynav-list">
+        <nav className="overflow-auto mynav-nav">
+          <ul className="d-flex gap-3 mb-1 mynav-list">
             <li className="mynav-li">
               <h6 className="text-white text-center mb-0">Primi</h6>
             </li>
