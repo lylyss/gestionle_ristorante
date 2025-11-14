5f2908f08b641a29a90add37ff1a40004a2440d1
Ren-Mor <renatomorelli@hotmail.it>
2025-11-14 16:00:27 +0100

    fix stile pagina

--- gestionale_ristorante/src/components/MyHero.tsx
+++ gestionale_ristorante/src/components/MyHero.tsx
@@ -8,12 +8,12 @@ function Hero() {
   return (
     <>
       <section className="text-border hero-background text-white d-flex flex-column justify-content-center align-items-center">
-        <h1 className="text-center">Benvenuti al Bistrot</h1>
+        <h1 className="display-2 text-center">Benvenuti al Bistrot</h1>
       </section>
       <Container>
         <div className="fixed-bottom d-flex justify-content-center mb-5">
-          <NavLink className="mb-4" to="/menupage">
-            <Button className="btn btn-secondary mb-5 fs-4">Sfoglia il menu</Button>
+          <NavLink className="mb-5" to="/menupage">
+            <Button className="btn btn-secondary mb-5 fs-4">Scopri il menu</Button>
           </NavLink>
         </div>
       </Container>
