6433916c01e011b9b3300aabf450f9ece2ef7888
Ren-Mor <renatomorelli@hotmail.it>
2025-11-18 11:21:48 +0100

    Struttura iniziale pagina menu

--- gestionale_ristorante/src/App.tsx
+++ gestionale_ristorante/src/App.tsx
@@ -1,11 +1,13 @@
 import HomePage from "./pages/HomePage";
 import "./App.css";
 import AuthPage from "./pages/AuthPage";
+import MenuPage from "./pages/MenuPage";
 
 function App() {
   return (
     <>
       <HomePage />
+      <MenuPage />
       <AuthPage />
     </>
   );
