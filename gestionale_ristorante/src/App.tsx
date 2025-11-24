f160cd6538bc16811aba7f3b02f76c35d8dce0ab
Loboda Nicolae <77647358+lylyss@users.noreply.github.com>
2025-11-24 16:38:01 +0100

    Refactor App component to use React Router for navigation

--- gestionale_ristorante/src/App.tsx
+++ gestionale_ristorante/src/App.tsx
@@ -3,14 +3,18 @@ import "./App.css";
 import AuthPage from "./pages/AuthPage";
 import MenuPage from "./pages/MenuPage";
 import BackOfficePage from "./pages/BackOfficePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <HomePage />
-      <MenuPage />
-      <AuthPage />
-      <BackOfficePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/backoffice" element={<BackOfficePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
