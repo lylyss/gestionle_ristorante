f1349fe1ee20f59fde285bc569b5398a730e7c67
Loboda Nicolae <77647358+lylyss@users.noreply.github.com>
2025-11-24 16:41:28 +0100

    Remove BrowserRouter wrapper from App component for simplified routing structure

--- gestionale_ristorante/src/App.tsx
+++ gestionale_ristorante/src/App.tsx
@@ -3,18 +3,16 @@ import "./App.css";
 import AuthPage from "./pages/AuthPage";
 import MenuPage from "./pages/MenuPage";
 import BackOfficePage from "./pages/BackOfficePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/backoffice" element={<BackOfficePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/menu" element={<MenuPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/backoffice" element={<BackOfficePage />} />
+    </Routes>
   );
 }
 
