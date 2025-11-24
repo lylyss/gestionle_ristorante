b1f8474ebc606a60ca8f6c9b4ecce0a0b409d22e
Ren-Mor <renatomorelli@hotmail.it>
2025-11-24 16:47:04 +0100

    aggiunta route

--- gestionale_ristorante/src/App.tsx
+++ gestionale_ristorante/src/App.tsx
@@ -1,6 +1,7 @@
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import MenuPage from "./pages/MenuPage";
+import BackOfficePage from "./pages/BackOfficePage";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/menu" element={<MenuPage />} />
+        <Route path="/backoffice" element={<BackOfficePage />} />
       </Routes>
     </>
   );
